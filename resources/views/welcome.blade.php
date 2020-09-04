483a3d18753d98374b462b9e74304fc95ae9ec4c
root <root@vps293607.vps.ovh.ca>
2020-09-04 17:56:38 -0400

    Modified welcome page.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Colby McHenry
                 </div>
 
                 <div class="links">
